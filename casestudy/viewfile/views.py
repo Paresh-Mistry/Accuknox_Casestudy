3e69203c6bf24ed58c4e90e143dda65f73602783
Paresh-Mistry <pareshkmistry04@gmail.com>
2025-02-25 19:33:05 +0530

    Add Project

--- casestudy/viewfile/views.py
+++ casestudy/viewfile/views.py
@@ -1,5 +1,4 @@
 from django.db import transaction
-from django.http import HttpResponse
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -7,37 +6,30 @@ import time
 import threading
 
 # Question 1: By default, are Django signals executed synchronously or asynchronously?
-# @receiver(post_save, sender=User)
-# def my_signal_handler_1(sender, instance, **kwargs):
-#     print("Signal received, processing...")
-#     time.sleep(3)  # Simulating a delay
-#     print("Signal processing completed.")
-# new_user = User.objects.create(username="test_user")
-# print("User creation completed.")
-
-
-def home(request):
-    try:
-     return HttpResponse("this is django file")
-    except:
-       print('error')
+@receiver(post_save, sender=User)
+def my_signal_handler_1(sender, instance, **kwargs):
+    print("Signal received, processing...")
+    time.sleep(3)  # Simulating a delay
+    print("Signal processing completed.")
+new_user = User.objects.create(username="test_user")
+print("User creation completed.")
 
 
 # Question 2: Do Django signals run in the same thread as the caller?
-# @receiver(post_save, sender=User)
-# def my_signal_handler1(sender, instance, **kwargs):
-#     print(f"Signal running in thread: {threading.current_thread().name}")
-# print(f"Main thread: {threading.current_thread().name}")
-# new_user = User.objects.create(username="test_user")
+@receiver(post_save, sender=User)
+def my_signal_handler1(sender, instance, **kwargs):
+    print(f"Signal running in thread: {threading.current_thread().name}")
+print(f"Main thread: {threading.current_thread().name}")
+new_user = User.objects.create(username="test_user")
 
 
 # Question 3: By default, do Django signals run in the same database transaction as the caller?
-# @receiver(post_save, sender=User)
-# def my_signal_handler2(sender, instance, **kwargs):
+@receiver(post_save, sender=User)
+def my_signal_handler2(sender, instance, **kwargs):
     print(f"Signal inside transaction: {transaction.get_connection().in_atomic_block}")
 
 # Test: Creating a new user inside a transaction
-# with transaction.atomic():
-#     new_user = User.objects.create(username="test_user")
+with transaction.atomic():
+    new_user = User.objects.create(username="test_user")
 
 
